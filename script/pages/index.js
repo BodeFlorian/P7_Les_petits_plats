650b5b9f9deef8098957f73a9e50ba775cfa4164
Florian Bode <florianbode59@gmail.com>
2024-04-30 21:57:24 +0200

    Utilisation de 'for' dans searchRecipes (filtre)

--- script/pages/index.js
+++ script/pages/index.js
@@ -296,24 +296,54 @@ const allUstensils = (recipes) => {
  * @param {Array} x - Tableau de filtres
  * @returns {Array} - Tableau des recettes filtrées
  */
-const searchRecipes = (x) => {
-  return filteredRecipes.filter((recipe) => {
-    // Pour chaque filtre, vérifiez si la recette correspond
-    return x.every((filter) => {
-      filter = filter.toLowerCase();
-      return (
-        recipe.name.toLowerCase().includes(filter) ||
-        recipe.description.toLowerCase().includes(filter) ||
-        recipe.appliance.toLowerCase().includes(filter) ||
-        recipe.ustensils.some((ustensil) =>
-          ustensil.toLowerCase().includes(filter)
-        ) ||
-        recipe.ingredients.some((ingredient) =>
-          ingredient.ingredient.toLowerCase().includes(filter)
-        )
-      );
-    });
-  });
+const searchRecipes = (filters) => {
+  const filtered = [];
+  const filtersLowerCase = filters.map((filter) => filter.toLowerCase()); //On utilise lowercase pour filtrer sans problème.
+
+  for (let i = 0; i < filteredRecipes.length; i++) {
+    const recipe = filteredRecipes[i];
+    let match = true;
+
+    for (let j = 0; j < filtersLowerCase.length; j++) {
+      const filter = filtersLowerCase[j];
+
+      if (
+        !recipe.name.toLowerCase().includes(filter) &&
+        !recipe.description.toLowerCase().includes(filter) &&
+        !recipe.appliance.toLowerCase().includes(filter)
+      ) {
+        let hasIngredient = false;
+        let hasUstensil = false;
+
+        for (let k = 0; k < recipe.ingredients.length; k++) {
+          if (recipe.ingredients[k].ingredient.toLowerCase().includes(filter)) {
+            hasIngredient = true;
+            break;
+          }
+        }
+
+        if (!hasIngredient) {
+          for (let l = 0; l < recipe.ustensils.length; l++) {
+            if (recipe.ustensils[l].toLowerCase().includes(filter)) {
+              hasUstensil = true;
+              break;
+            }
+          }
+        }
+
+        if (!hasIngredient && !hasUstensil) {
+          match = false;
+          break;
+        }
+      }
+    }
+
+    if (match) {
+      filtered.push(recipe);
+    }
+  }
+
+  return filtered;
 };
 
 const init = () => {
